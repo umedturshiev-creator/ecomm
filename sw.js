970ecc6277975b2a9d4fb187940db7b1857abc0a
umedturshiev-creator <umedturshiev@gmail.com>
2026-05-25 02:07:52 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'smartpay-crm-v1.6-mobile-top-clean-20260524-01';
+const CACHE_VERSION = 'smartpay-crm-v1.6-profile-visibility-20260524-02';
 const APP_SHELL = [
   './',
   './index.html',
