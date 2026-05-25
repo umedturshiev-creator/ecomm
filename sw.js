a9603feccfc4a472873cd53e8093345bd8da2e75
umedturshiev-creator <umedturshiev@gmail.com>
2026-05-25 08:04:16 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='smartpay-crm-v1-6-form-visibility-fix';
+const CACHE_NAME='smartpay-crm-v1-6-merchant-card-visibility-fix';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))).then(()=>self.clients.claim()));});
 self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting();});
