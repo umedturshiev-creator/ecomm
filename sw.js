5835b0d80520601a88e73a089c4b0b2bb132bbfa
umedturshiev-creator <umedturshiev@gmail.com>
2026-06-07 18:50:19 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='smartpay-crm-v2-0-settings-profile-tj';
+const CACHE_NAME='smartpay-crm-v2-1-dashboard-high-priority-scroll';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))).then(()=>self.clients.claim()));});
 self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting();});
