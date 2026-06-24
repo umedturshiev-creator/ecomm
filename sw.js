a938695a48b37f1f64b8a21ebc6de97e5bc98ad6
umedturshiev-creator <umedturshiev@gmail.com>
2026-06-24 23:42:32 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,5 @@
-const CACHE_NAME='smartpay-crm-v3-5-4-chat-password-front-fix';
-self.addEventListener('install',event=>{
-  self.skipWaiting();
-});
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.map(key=>caches.delete(key))))
-      .then(()=>self.clients.claim())
-  );
-});
-self.addEventListener('message',event=>{
-  if(event.data&&event.data.type==='SKIP_WAITING') self.skipWaiting();
-});
-self.addEventListener('fetch',event=>{
-  const req=event.request;
-  event.respondWith(fetch(req,{cache:'no-store'}).catch(()=>caches.match(req)));
-});
+const CACHE_NAME='smartpay-crm-v3-5-6-read-receipts-emoji';
+self.addEventListener('install',event=>{self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
+self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting();});
+self.addEventListener('fetch',event=>{const req=event.request;event.respondWith(fetch(req,{cache:'no-store'}).catch(()=>caches.match(req)));});
